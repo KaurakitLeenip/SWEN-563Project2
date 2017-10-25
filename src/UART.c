83c8b0ce4142ac5d0eea491378ac398d04903119
Kaurakit <kxl7131@rit.edu>
2017-10-25 12:08:34 -0400

    added a quicker return when override is cancelled

--- src/UART.c
+++ src/UART.c
@@ -109,7 +109,7 @@ void Read_Line(char * str){
 	int index = 1;
 	//index here is 1 as the first character is read using USART_Read as it trips the RXNE flag
 	
-	while( input_byte != '>' ){
+	while( input_byte != '>' || input_byte != 'x' || input_byte != 'X'){
 		input_byte = USART_Read(USART2);
 		str[index] = input_byte;
 		index++;
